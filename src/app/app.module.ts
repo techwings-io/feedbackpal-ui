8b006a66d3495b33f633bbaac7b19d185d5ac8d3
Marco Tedone <marco@techwings.io>
2020-12-13 17:16:43 +0000

    Cookie consetn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,22 @@ import {
   NgcCookieConsentModule,
 } from 'ngx-cookieconsent';
 
+const cookieConfig: NgcCookieConsentConfig = {
+  cookie: {
+    domain: 'localhost', // or 'your.domain.com' // it is mandatory to set a domain, for cookies to work properly (see https://goo.gl/S2Hy2A)
+  },
+  palette: {
+    popup: {
+      background: '#000',
+    },
+    button: {
+      background: '#f1d600',
+    },
+  },
+  theme: 'edgeless',
+  type: 'info',
+};
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -51,6 +67,7 @@ import {
     }),
     BrowserAnimationsModule,
     NgxPaginationModule,
+    NgcCookieConsentModule.forRoot(cookieConfig),
   ],
   providers: [
     {
